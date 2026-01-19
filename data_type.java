4412f53fb47614b72222ae4c705dbf0519608b68
121Siddhesh <mawalesiddhesh67@gmail.com>
2026-01-19 20:51:35 +0530

    first commit

---
+++ data_type.java
@@ -0,0 +1,26 @@
+class data{
+    public static void main(String a[]){
+        int num =10;
+        byte b = 127;
+        short sh= 558;
+        long lg =5854l;
+
+        float fl =6.5f;
+        double d = 6.6;
+        
+        char ch ='A';
+
+        boolean bl =true;
+
+        System.out.println(num);
+        System.out.println(b);
+        System.out.println("Short value: " + sh);
+        System.out.println("long value: " + lg);
+        System.out.println("float value: " + fl);
+        System.out.println("double value: " + d);
+        System.out.println("char value: " + ch);
+        System.out.println("boolean value: " + bl);
+           
+
+    }
+}
