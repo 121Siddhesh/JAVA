5c30fd73b60dad752c8ea67231b85b25a75f52f8
121Siddhesh <mawalesiddhesh67@gmail.com>
2026-01-20 21:16:58 +0530

    second commit

---
+++ literal.java
@@ -0,0 +1,13 @@
+class literal{
+    public static void main(String a[]){
+        int num=0b1010; //binary literal
+        System.out.println("Binary literal " + num);
+
+        int num1 =0x1A; //hexadecimal literal
+        System.out.println("Hexadecimal literal " + num1);
+
+        char ch ='a';
+        ch++;
+        System.out.println("Character literal after increment: " + ch);
+    }
+}
